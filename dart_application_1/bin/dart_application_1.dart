2027a77b537c62e5fdaf8d79b87804fa30d5761a
LucasZornoff <lucas.clal@gmail.com>
2023-06-01 17:28:21 -0300

    avancando na aplicação

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,10 +1,20 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
 void main() {
-  int idade = 0x0000019;
-  idade ++;
+  int idade = 25;
+  double altura = 1.74;
+  bool geek = true;
+  String nome = 'Lucas Zornoff Clal';
+  String apelido = 'Farmácia';
+  
+  List<String> listanomes = ['Ricarth', 'Natalia', 'Alex', 'Ândriu', 'André'];
+  List<dynamic> farmacia = [27, 1.75, true, 'Lucas Zornoff Clal', 'Farmacia'];
 
+  String frase = 'Eu sou $farmacia[4] \n'
+      'mas meu nome completo é: $nome \n'
+      'Eu me considero geek? $geek \n'
+      'Eu tenho $altura metros de altura e \n'
+      '$idade anos de idade';
 
-
-  print(idade);
+  print(listanomes);
 }
